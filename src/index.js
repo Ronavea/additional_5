3ddb85a3c8fcf729cf70ca7eecb528694e42ccf8
Pavel <eviedrande@gmail.com>
2018-09-11 11:57:50 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-	let mas = "";
-	let stroka = str;
-	
-	for(let n=0; n<=bracketsConfig.length; n++){		//перезапуск цикла
-		for (let i=0; i<bracketsConfig.length; i++){
-			for (let j=0; j<2; j++){
-			mas += bracketsConfig[i][j];				//запись скобок в переменную
-			}	
-			for(let k=0; k<str.length/2; k++){			//при совпадение скобок
-				stroka = stroka.replace( mas, "");		//удаление скобок в строке		
-			}
-			mas = ""; 
-		}	
+  let mas = "";
+  let stroka = str;
+  let n = bracketsConfig.length + 1;
+  
+  while(n--) {
+	for(let i = 0; i < bracketsConfig.length; i++) {
+	  for(let j = 0; j < 2; j++) {
+		mas += bracketsConfig[i][j];                  //запись скобок в переменную
+	  }
+
+	  for(let k = 0; k < str.length/2; k++) {         //при совпадении скобок
+		stroka = stroka.replace(mas, "");             //удаление скобок в строке
+	  }
+	  mas = "";
 	}
-	if (stroka == ""){
-		return true;
-	}	
-	else {
-		return false;
-	}
+  }
+
+  return (stroka == "") ? true : false;
 }
